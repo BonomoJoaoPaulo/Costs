89ccfbd7769e23faad5faac9ca4e7fc7af29c046
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-02-04 15:07:51 -0300

    Aula #23

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,9 +1,29 @@
+import {useEffect, useState} from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
+
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+       method: "GET",
+       headers: {
+           'Content-Type':'application/json',
+       },
+    })
+       .then((resp) => resp.json())
+       .then((data) => {
+           setCategories(data)
+       })
+       .catch((err) => console.log(err))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input type="text"
@@ -16,7 +36,9 @@ function ProjectForm({btnText}) {
             name="budget"
             placeholder="Insira o orçamento total"
             />
-            <Select name="category-id" text="Selecione a categoria"
+            <Select name="category-id"
+            text="Selecione a categoria"
+            options={categories}
             />
             <SubmitButton text={btnText}
             />
