d847a3855c95611f81bdc35f6a6819272ecbb6e2
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-02-04 11:09:26 -0300

    Aula #18

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,9 +1,35 @@
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
+
 function App() {
     return (
-        <div className="App">
-            <p>Costs</p>
-        </div>
-    )
-}
+      <Router> 
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/Company">Empresa</Link>
+          <Link to="/Contact">Contato</Link>
+          <Link to="/NewProject">Novo Projeto</Link>
+        </ul>
+  
+          <Container customClass="min-height">
+            <Routes>
+              <Route path= "/" element={<Home/>}/> 
+              <Route path= "/Company" element={<Company/>}/> 
+              <Route path= "/Contact" element={<Contact/>}/> 
+              <Route path= "/NewProject" element={<NewProject/>}/>    
+            </Routes>
+          </Container>
+  
+        <p>Footer</p>
+  
+      </Router>
+    );
+  }
 
 export default App
---
+++ costs/src/components/pages/NewProject.js
@@ -0,0 +1,9 @@
+function NewProject() {
+    return (
+        <div>
+            <h1>Novo Projeto</h1>
+        </div>
+    )
+}
+
+export default NewProject
