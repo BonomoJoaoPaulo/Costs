ee93cc6c9facff48a98e0d700fb5e61b4ba940cd
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-02-08 14:22:48 -0300

    Criando Loader - aula 28

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -5,12 +5,14 @@ import Message from "../layout/Message"
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
+import Loading from '../layout/Loading'
 
 import styles from "./Projects.module.css"
 
 function Projects() {
 
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
     
     const location = useLocation()
     let message = ''
@@ -19,15 +21,20 @@ function Projects() {
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             },
         })
         .then((resp) => resp.json())
-        .then((data) => {setProjects(data)})
+        .then((data) => {
+            setProjects(data)
+            setRemoveLoading(true)
+        })
         .catch((err) => console.log(err))
+        }, 1000)
     }, [])
 
     return (
@@ -39,7 +46,7 @@ function Projects() {
             {message && <Message type="success" msg={message} />}
             <Container customClass="start">
                 {projects.length > 0 &&
-                projects.map((project) =>
+                projects.map((project) => (
                 <ProjectCard
                     id={project.id}
                     name={project.name}
@@ -47,6 +54,10 @@ function Projects() {
                     category={project.category.name}    
                     key={project.id}
                 />
+                ))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
                 )}
             </Container>
         </div>
