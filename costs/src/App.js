53bbe605f6d40d385aeba5736c3cc8b1480d5702
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-02-08 14:56:58 -0300

    Página de edição de dados - aula 30

--- costs/src/App.js
+++ costs/src/App.js
@@ -5,6 +5,7 @@ import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 import Projects from './components/pages/Projects'
+import Project from './components/pages/Project'
 
 import Container from './components/layout/Container'
 import NavBar from './components/layout/NavBar'
@@ -20,11 +21,12 @@ function App() {
   
           <Container customClass="min-height">
             <Routes>
-              <Route path= "/" element={<Home/>}/> 
+              <Route exact path= "/" element={<Home/>}/> 
               <Route path= "/Company" element={<Company/>}/> 
               <Route path= "/Contact" element={<Contact/>}/>
               <Route path= "/Projects" element={<Projects/>}/> 
-              <Route path= "/NewProject" element={<NewProject/>}/>    
+              <Route path= "/NewProject" element={<NewProject/>}/>
+              <Route path= "/Project/:id" element={<Project/>}/>     
             </Routes>
           </Container>
           <Footer />
