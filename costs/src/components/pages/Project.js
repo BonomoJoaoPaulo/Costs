dff51885353fb9cf7f2c958c31b73e78ca77de8c
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-02-08 16:30:37 -0300

    Criação da área de serviços

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -13,6 +13,7 @@ function Project() {
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -32,6 +33,7 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
         //budget validation
         if(project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto.')
@@ -58,6 +60,10 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
@@ -91,6 +97,19 @@ function Project() {
                             </div>
                         )}
                     </div>
+                    <div className={styles.service_form_container}>
+                        <h2>Adicione um serviço:</h2>
+                        <button className={styles.btn} onClick={toggleServiceForm}>
+                        {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                        </button>
+                        <div className={styles.project_info}>
+                            {showServiceForm && <div>Formulário do serviço</div>}
+                        </div>
+                    </div>
+                    <h2>Serviços</h2>
+                    <Container customClass="start">
+                        <p>Itens de serviços</p>
+                    </Container>
                 </Container>
             </div>
             ) : (<Loading />)}
